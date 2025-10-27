21a859fcc5af05d326c94a9523c50295c0ec3542
Perumalla Sravanthi <sravanthip750@gmail.com>
2025-10-27 10:38:16 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,1 @@
-<?php
-session_start();
 
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
-
-if ($username === 'admin' && $password === 'admin123') {
-    $_SESSION['admin'] = true;
-    header("Location: dashboard.php");
-    exit();
-} else {
-    echo "<script>alert('Invalid login'); window.location.href = 'login.html';</script>";
-}
-?>
